486bc4b0d51caae0ace3665fb6d0a7b17b03ff9a
mijoycelam <mijoycelam@gmail.com>
2024-05-06 14:26:59 +0200

    fix: insert timeout

--- webapp/Frontend/akros-frontend/src/app/site_Steps.ts
+++ webapp/Frontend/akros-frontend/src/app/site_Steps.ts
@@ -4,17 +4,17 @@ import { expect, chromium, Page, Browser } from '@playwright/test';
 let page: Page;
 let browser: Browser;
 
-Given('I am on the home page', async () => {
+Given('I am on the home page', {timeout: 10000}, async () => {
   browser = await chromium.launch({ headless: false });
   page = await browser.newPage();
   await page.goto('http://localhost:4200/');
 });
 
-When('I click on the button with ID {string}', async (buttonId: string) => {
+When('I click on the button with ID {string}', {timeout: 10000}, async (buttonId: string) => {
   await page.click(`#${buttonId}`);
 });
 
-Then('I should be navigated to {string}', async (expectedUrl: string) => {
+Then('I should be navigated to {string}',{timeout: 10000}, async (expectedUrl: string) => {
   await page.waitForNavigation();
   expect(page.url()).toBe(expectedUrl);
   await browser.close();
